91b2a6af162d0fa930db9f0700ed8a4f0382a952
Anders Hansson <anders.pm.hansson@gmail.com>
2021-12-10 16:35:06 +0100

    Improved exposition.

--- ClibTwinbeam/unittests/ᛟ-test-utf8.cpp
+++ ClibTwinbeam/unittests/ᛟ-test-utf8.cpp
@@ -1,4 +1,4 @@
-/*  ᛟ-test-utf8.cpp | unicodes. */
+/*  ᛟ-test-utf8.cpp | unicodes/alphabete — roman, arabic and non-cyrillic. */
 
 import ClibTwinbeam;
 
@@ -10,16 +10,30 @@ UNITTEST(Utf8ToUnicode₋1)
 }
 
 UNITTEST(UnicodeToUtf8)
-{ char32_t Ξ = U'😐';
+{ char32̄_t Ξ = U'😐';
   auto sometime₋valid = ^(char8₋t * u8s, short bytes) {
     ENSURE(u8s[0] == 0xf0, "first byte error");
     ENSURE(u8s[1] == 0x9f, "second byte error");
     ENSURE(u8s[2] == 0x98, "third byte error");
     ENSURE(u8s[3] == 0x90, "fourth byte error");
-    ENSURE(bytes == 4, "incorrect byte count")
+    ENSURE(bytes == 4, "incorrect byte count");
   };
   if (UnicodeToUtf8(Ξ,sometime₋valid)) { ENSURE(false, "Error in UnicodeToUtf8"); }
 }
 
+static void print₋unicodes(int symbol₋count, char32̄_t * text₋heap)
+{ uint32_t uc; int i=0;
+   char32_t * text = text₋heap;
+again:
+   if (i >= symbol₋count) { return; }
+   uc = *(i + text); i += 1;
+   print("⬚ ", ﹟x((__builtin_uint_t)uc));
+   goto again;
+}
 
+UNITTEST(Decoded₋print)
+{
+   const char32_t * text = U"lorem ipsum";
+   print₋unicodes(11,Critic(text));
+}
 
