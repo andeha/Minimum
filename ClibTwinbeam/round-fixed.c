70d7a94ce99266264763fe10a992278a0cf75ffb
Anders Hansson <anders.pm.hansson@gmail.com>
2021-12-21 15:08:58 +0100

    Improved exposition.

--- ClibTwinbeam/round-fixed.c
+++ ClibTwinbeam/round-fixed.c
@@ -205,7 +205,7 @@ void print₋sequent(struct sequent 𝕏,
     s = 10*(s % unity.detail.frac);
     delta *= 10;
    } while (s <= delta);
-   integers.bytes=i, integers.keyputs=ints, fracts.bytes=j, fracts.keyputs=fts;
+   integers.count=i, integers.keyputs=ints, fracts.count=j, fracts.keyputs=fts;
    digits(neg,integers,fracts);
 } /* #define EN₋TVÅ₋SUCCESSION₋AUTO₋OCH₋API / * b7874fba */
 
