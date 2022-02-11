57766dd10a6784a0071f1b3c9550f6a4e58080d6
Anders Hansson <anders.pm.hansson@gmail.com>
2022-02-11 23:06:20 +0100

    Improved exposition.

--- ClibTwinbeam/unittests/ᛟ-test-keying.cpp
+++ ClibTwinbeam/unittests/ᛟ-test-keying.cpp
@@ -8,12 +8,12 @@ UNITTEST(Rope₋keyput₋1)
    struct two₋memory dynmem = { Heap₋unalloc, Heap₋unalloc, Heap₋alloc, Heap₋alloc,
     Heap₋object₋size };
    struct Unicodes text1 = { 11, UC("hello world") }, text2 = { 8, UC(" thesis ") };
-   unicode₋shatter shat1=branch₋to₋shatter(text1), shat2=branch₋to₋shatter(text2);
+   unicode₋shatter shat1=copy₋to₋shatter(text1), shat2=copy₋to₋shatter(text2);
    if (rope₋append₋text(&opaque1,shat1,dynmem)) { ENSURE(false, "append-1 failed"); }
    if (rope₋append₋text(&opaque2,shat2,dynmem)) { ENSURE(false, "append-2 failed"); }
    if (rope₋insert(&opaque1,5,&opaque2,dynmem)) { ENSURE(false, "insert failed"); }
    __builtin_int_t count = rope₋length(opaque1);
-   char c = rope₋index(opaque1,7);
+   char32̄_t uc = rope₋index(opaque1,7);
    if (rope₋delete(&opaque1,11,2,dynmem)) { ENSURE(false, "delete failed"); }
    unalloc₋rope(opaque1,dynmem);
    unalloc₋rope(opaque2,dynmem);
