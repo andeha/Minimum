04419c2b6531367d33cc011ae74e30accf3b8a9d
Anders Hansson <anders.pm.hansson@gmail.com>
2022-02-23 07:28:20 +0100

    ClibTwinbeam/big-concrete.c

---
+++ ClibTwinbeam/ketter-bowler.c
@@ -0,0 +1,34 @@
+/*  ketter-bowler.c | thesarus a․𝘬․a associative map with right-hand array. */
+
+import ClibTwinbeam;
+
+int form₋ōnymon(struct Unicodes key, struct Unicodes val, int shares, 
+ void ᶿ﹡* opaque, ALLOC alloc)
+{
+   INIT init₋with₋structa = ^(void * uninited) { return structa₋init(4, 
+    4096,uninited,alloc); }; /* ⬷ rope-pointers stored. */
+   int size = sizeof(struct structa);
+   struct structa * list = jot(key,opaque,size,alloc,init₋with₋structa);
+   return 0;
+}
+
+int dissociate₋isolate(struct Unicodes key, int idx, void ᶿ﹡* opaque)
+{
+   return 0;
+}
+
+int dissociate₋all(struct Unicodes key, void ᶿ﹡* opaque)
+{
+   return 0;
+}
+
+int evidence₋related(struct Unicodes key, void (^right)(int count, 
+  struct Unicodes val[]), void ᶿ﹡* opaque
+)
+{
+   __uint128_t fineprint = FNV1b(4*key.tetras, (void *)key.unicodes);
+   struct w₋node * node = (struct w₋node *)impression₋seek(*opaque,fineprint);
+   struct structa * list = (struct structa *)node->note;
+   return 0;
+}
+
