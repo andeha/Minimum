e7a2fb07691f0e01c48faabe6f8eaf7a8396e2b9
Anders Hansson <anders.pm.hansson@gmail.com>
2022-02-23 19:00:04 +0100

    Improved exposition.

--- ClibTwinbeam/ketter-bowler.c
+++ ClibTwinbeam/ketter-bowler.c
@@ -1,14 +1,24 @@
-/*  ketter-bowler.c | thesarus a․𝘬․a 'associative map with right-hand array'. */
+/*  ketter-bowler.c | thesarus a․𝘬․a associative map with right-hand array. */
 
 import ClibTwinbeam;
 
 int form₋ōnymon(struct Unicodes key, struct Unicodes val, int shares, 
  void ᶿ﹡* opaque, ALLOC alloc)
-{
-   INIT init₋with₋structa = ^(void * uninited) { return structa₋init(4, 
+{ int size=sizeof(struct structa), szelem=sizeof(union Tetra𝘖rUnicode);
+   INIT init₋with₋structa = ^(void * uninited) { return structa₋init(szelem, 
     4096,uninited,alloc); }; /* ⬷ rope-pointers stored. */
-   int size = sizeof(struct structa);
    struct structa * list = jot(key,opaque,size,alloc,init₋with₋structa);
+   if (list == ΨΛΩ) { return -1; }
+   return 0;
+}
+
+inexorable int cross₋item(int idx, void * opaque)
+{
+   return 0;
+}
+
+inexorable int item₋is₋crossed(int idx, void * opaque)
+{
    return 0;
 }
 
@@ -26,8 +36,9 @@ int evidence₋related(struct Unicodes key, void (^right)(int count,
   struct Unicodes val[]), void ᶿ﹡* opaque
 )
 {
-   __uint128_t fineprint = FNV1b(4*key.tetras, (void *)key.unicodes);
+   __uint128_t fineprint = FNV1b(4*key.tetras,(void *)key.unicodes);
    struct w₋node * node = (struct w₋node *)impression₋seek(*opaque,fineprint);
+   if (*opaque == ΨΛΩ) { *opaque=node; }
    struct structa * list = (struct structa *)node->note;
    return 0;
 }
