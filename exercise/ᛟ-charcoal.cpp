1d74a77ebf4a14c72824025df3b6aa097fd73565
Anders Hansson <anders.pm.hansson@gmail.com>
2022-02-25 10:02:41 +0100

    Improved exposition.

--- exercise/ᛟ-charcoal.cpp
+++ exercise/ᛟ-charcoal.cpp
@@ -6,12 +6,16 @@ UNITTEST(charcoal₋grammar₋1)
 {  struct Unicodes program = UC("width 10 cm, height 10 cm; ");
    int width, height; if (parse₋art₋system(program,&width,&height)) 
    { ENSURE(false, "error in parse-art-system."); }
-   Linewidth linewidth = ^(double width) { mfprint("linewidth "); };
-   Color color = ^(double c, double m, double y, double blk, double a) { mfprint("color "); };
+   Linewidth linewidth = ^(double width) { mfprint("linewidth ⬚ ",﹟F(width,1)); };
+   Color color = ^(double c, double m, double y, double blk, double a) { 
+    mfprint("color ⬚,⬚,⬚,⬚,⬚",﹟F(c,1),﹟F(m,1),﹟F(y,1),﹟F(blk,1),﹟F(a,1)); };
    Begin begin = ^{ mfprint("begin "); };
-   Move move = ^(double x, double y) { mfprint("move "); };
-   Addcurve addcurve = ^(double x[], double y[]) { mfprint("addcurve "); };
-   Addstraight addstraight = ^(double x, double y) { mfprint("addstraight "); };
+   Move move = ^(double x, double y) { mfprint("move ⬚,⬚", ﹟F(x,1),﹟F(y,1)); };
+   Addcurve addcurve = ^(double x[], double y[]) { mfprint("addcurve ["); 
+    for (int i=0; i<3; i+=1) { print("⬚,⬚; ",﹟F(x[i],1),﹟F(y[i],1)); } 
+    mfprint("] "); };
+   Addstraight addstraight = ^(double x, double y) { 
+    mfprint("addstraight ⬚,⬚", ﹟F(x,1),﹟F(y,1)); };
    Closepath closepath = ^{ mfprint("closepath "); };
    Stroke stroke = ^{ mfprint("stroke "); };
    if (draw₋art₋system(program,color,linewidth,begin,move,addcurve,addstraight,closepath,stroke)) 
