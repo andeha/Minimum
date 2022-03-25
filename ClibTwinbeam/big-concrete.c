f64dff8178143d4a695f474bb64310ec160a9543
Anders Hansson <anders.pm.hansson@gmail.com>
2022-03-25 13:39:00 +0100

    Improved exposition.

--- ClibTwinbeam/big-concrete.c
+++ ClibTwinbeam/big-concrete.c
@@ -1,4 +1,4 @@
-/*  big-concrete.c | a 128-bit search tree. */
+/*  big-concrete.c | not longer than 128-bit search tree. */
 
 import ClibTwinbeam;
 
@@ -13,50 +13,46 @@ __uint128_t FNV1b(__builtin_int_t bytes, void * material)
     return h;
 }
 
-inexorable struct w₋node * new₋node(__uint128_t fineprint, ALLOC alloc)
+inexorable struct w₋node * new₋node(__uint128_t fineprint, void * material, ALLOC alloc)
 {
    struct w₋node * node = (struct w₋node *)alloc(sizeof(struct w₋node));
    if (node == ΨΛΩ) { return ΨΛΩ; }
-   node->key = fineprint; node->left=ΨΛΩ; node->right=ΨΛΩ;
+   node->key = fineprint; node->left=ΨΛΩ; node->right=ΨΛΩ; node->material=material;
    return node;
 }
 
-void * impression₋seek(void ᶿ﹡ opaque, __uint128_t target)
+struct w₋node * impression₋seek(void ᶿ﹡ opaque, __uint128_t target)
 { struct w₋node * node = (struct w₋node *)opaque;
    if (node == ΨΛΩ) { return ΨΛΩ; }
    else { if (target == node->key) { return node; }
      if (target < node->key) return impression₋seek(node->left,target);
      else return impression₋seek(node->right,target);
    }
-}
+} /* ⬷ returns the found node alternatively an empty pointer. */
 
-void * impression₋store(void ᶿ﹡ opaque, __uint128_t fineprint, ALLOC alloc)
+struct w₋node * impression₋store(void ᶿ﹡ opaque, __uint128_t fineprint, 
+ void * material, ALLOC alloc)
 { struct w₋node * node = (struct w₋node *)opaque;
-   if (opaque == ΨΛΩ) { return new₋node(fineprint,alloc); }
+   if (opaque == ΨΛΩ) { return new₋node(fineprint,material,alloc); }
    else {
      if (fineprint <= node->key) {
-       node->left = impression₋store(node->left,fineprint,alloc);
-     } else { node->right=impression₋store(node->right,fineprint,alloc); }
+       node->left = impression₋store(node->left,fineprint,material,alloc);
+     } else { node->right=impression₋store(node->right,fineprint,material,alloc); }
      return node;
    }
-}
-
-/* void * Impression₋store(void ᶿ﹡* opaque, __uint128_t fineprint, ALLOC alloc)
-{
-   struct w₋node * node = impression₋store(*opaque,fineprint,alloc);
-   if (*opaque == ΨΛΩ) { *opaque = node; }
-} */
+} /* ⬷ returns a reference to a newly created 'w₋node' in-case formerly 
+ empty tree else returns unchanged pointer to opaque which is a w-node. */
 
 notepointer jot(struct Unicodes token, void ᶿ﹡* opaque, __builtin_int_t 
  notebytes, ALLOC alloc, INIT init)
-{ __uint128_t fineprint=FNV1b(4*token.tetras,(void *)token.unicodes);
-  struct w₋node * node = (struct w₋node *)impression₋seek(*opaque,fineprint);
+{ __uint128_t fineprint=FNV1b(token.tetras*4,token.unicodes);
+  struct w₋node * node = impression₋seek(*opaque,fineprint);
   if (node == ΨΛΩ) {
-    node = (struct w₋node *)impression₋store(*opaque,fineprint,alloc);
-    node->note = alloc(notebytes);
-    if (init(node->note)) { return ΨΛΩ; }
+    void * note = alloc(notebytes);
+    if (init(note)) { return ΨΛΩ; }
+    *opaque = impression₋store(*opaque,fineprint,note,alloc);
+    return (notepointer)note;
   }
-  if (*opaque == ΨΛΩ) { *opaque=node; }
   return node->note;
 }
 
